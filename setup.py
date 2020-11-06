56c1eb25c10b60b925f142f96988a445b0ee3a70
Yusong Huang <yusongh@yahoo.com>
2020-11-06 08:41:28 -0800

    Added spaces on each side of the = signs.

--- setup.py
+++ setup.py
@@ -21,19 +21,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="seanblend",
-    version="0.0.1",
-    author="SeanBlend",
-    author_email="seanblend@gmail.com",
-    description="A Python module by SeanBlend.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/SeanBlend/SeanBlendModule",
-    py_modules=["seanblend"],
-    packages=setuptools.find_packages(),
-    install_requires=[
+    name = "seanblend",
+    version = "0.0.1",
+    author = "SeanBlend",
+    author_email = "seanblend@gmail.com",
+    description = "A Python module by SeanBlend.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/SeanBlend/SeanBlendModule",
+    py_modules = ["seanblend"],
+    packages = setuptools.find_packages(),
+    install_requires = [
     ],
-    classifiers=[
+    classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Operating System :: OS Independent",
