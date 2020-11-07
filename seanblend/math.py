2af8fcadc3fdd2ef37362b2e6f85b784545d8d6e
Yusong Huang <yusongh@yahoo.com>
2020-11-07 11:02:28 -0800

    Tried to get scientific notation to support negative,

--- seanblend/math.py
+++ seanblend/math.py
@@ -42,8 +42,12 @@ def ScientificNotation(num):
         return num
     elif num == 10:
         return "1x10^1"
-    elif num > 10:
-        while num >= 10:
-            num /= 10
-            revs += 1
+    elif num > 10 or num < 0:
+        while num >= 10 or num <= 0:
+            if num >= 10:
+                num /= 10
+                revs += 1
+            elif num <= 0:
+                num /= -10
+                revs == 1
         return str(num) + "x10^" + str(revs)
