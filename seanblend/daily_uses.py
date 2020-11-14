df32c45c3daeea48cbd223f4607e24ead458cf76
Yusong Huang <yusongh@yahoo.com>
2020-11-13 20:24:13 -0800

    fixed list bug.

--- seanblend/daily_uses.py
+++ seanblend/daily_uses.py
@@ -1,4 +1,4 @@
-def _PrintList(list):
+def _print_list(list):
     num = 1
     if len(list) != 0:
         print("\nYour list:")
@@ -12,23 +12,20 @@ def _PrintList(list):
         print("\nYour list is empty.\n")
 
 
-def List():
+def list():
     userList = []
-    userInput = str(input("Enter your item: "))    
-    
+    userInput = str(input("Enter your item: "))
+
     while userInput.lower() != "done":
         if userInput.lower() == "showlist":
-            _PrintList(userList)
+            _print_list(userList)
         elif userInput.lower().startswith("remove"):
             remove = userInput.lower().replace("remove", "").strip()
             if remove != "*":
                 userList.remove(remove)
-                continue
             else:
                 userList = []
-                continue
-        elif userInput.strip == 1:
-            userInput = "done"
-        userList.append(userInput)
+        else:
+            userList.append(userInput)
         userInput = str(input("Enter your item: "))
-    _PrintList(userList)
+    _print_list(userList)
