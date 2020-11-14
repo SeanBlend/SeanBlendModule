698602327b94c19d609facaf7d931f2735321071
Yusong Huang <yusongh@yahoo.com>
2020-11-13 17:02:21 -0800

    changed version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "seanblend",
-    version = "0.0.5",
+    version = "0.0.51",
     author = "SeanBlend",
     author_email = "seanblend@gmail.com",
     description = "A Python module by SeanBlend.",
