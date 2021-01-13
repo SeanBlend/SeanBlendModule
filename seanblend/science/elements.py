c09fe81f8a96fd8c67de6e0ee5c76f08f97b247e
SeanBlend <awesome.sean.haha@gmail.com>
2021-01-12 22:42:28 -0800

    added elements from hydrogen to lithium (1-3)

---
+++ seanblend/science/elements.py
@@ -0,0 +1,34 @@
+class Hydrogen:
+    def __init__(self):
+        self.name = "Hydrogen"
+        self.atomic_number = 1
+        self.atomic_mass = 1.0079
+        self.chemical_symbol = "H"
+        self.type = "Reactive Metals"
+
+    def data(self):
+        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
+
+
+class Helium:
+    def __init__(self):
+        self.name = "Helium"
+        self.atomic_number = 2
+        self.atomic_mass = 4.0026
+        self.chemical_symbol = "He"
+        self.type = "Mainly Non-Metals"
+
+    def data(self):
+        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
+
+
+class Lithium:
+    def __init__(self):
+        self.name = "Lithium"
+        self.atomic_number = 3
+        self.atomic_mass = 6.941
+        self.chemical_symbol = "Li"
+        self.type = "Reactive Metals"
+
+    def data(self):
+        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
