7ff589ae197baa1a3bb778f2c2a5bf580bd6c89c
SeanBlend <awesome.sean.haha@gmail.com>
2021-01-13 10:01:34 -0800

    changed styling for  1-4 elements

--- seanblend/science/elements.py
+++ seanblend/science/elements.py
@@ -1,46 +1,32 @@
-class Hydrogen:
-    def __init__(self):
-        self.name = "Hydrogen"
-        self.atomic_number = 1
-        self.atomic_mass = 1.0079
-        self.chemical_symbol = "H"
-        self.type = "Reactive Metals"
-
+class Element:
     def data(self):
         print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
 
 
-class Helium:
-    def __init__(self):
-        self.name = "Helium"
-        self.atomic_number = 2
-        self.atomic_mass = 4.0026
-        self.chemical_symbol = "He"
-        self.type = "Mainly Non-Metals"
+class Hydrogen(Element):
+    name = "Hydrogen"
+    chemical_symbol = "H"
+    atomic_mass = 1.0079
+    atomic_number = 1
+    type = "Reactive Metals"
 
-    def data(self):
-        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
+class Helium(Element):
+    name = "Helium"
+    chemical_symbol = "He"
+    atomic_mass = 4.0026
+    atomic_number = 2
+    type = "Mainly Non-metals"
 
+class Lithium(Element):
+    name = "Lithium"
+    chemical_symbol = "Li"
+    atomic_mass = 6.941
+    atomic_number = 3
+    type = "Reactive Metals"
 
-class Lithium:
-    def __init__(self):
-        self.name = "Lithium"
-        self.atomic_number = 3
-        self.atomic_mass = 6.941
-        self.chemical_symbol = "Li"
-        self.type = "Reactive Metals"
-
-    def data(self):
-        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
-
-
-class Beryllium:
-    def __init__(self):
-        self.name = "Beryllium"
-        self.atomic_number = 4
-        self.atomic_mass = 9.0122
-        self.chemical_symbol = "Be"
-        self.type = "Reactive Metals"
-        
-    def data(self):
-        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\ntype: {self.type}")
+class Beryllium(Element):
+    name = "Beryllium"
+    chemical_symbol = "Be"
+    atomic_mass = 9.0122
+    atomic_number = 4
+    type = "Reactive Metals"
