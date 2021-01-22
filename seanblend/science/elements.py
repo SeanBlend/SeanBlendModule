42709b6fd5694431f005ef0f3c3352c6ff6d91c1
SeanBlend <awesome.sean.haha@gmail.com>
2021-01-22 15:54:01 -0800

    added tantalum element (73rd) and changed type to series

--- seanblend/science/elements.py
+++ seanblend/science/elements.py
@@ -1,6 +1,6 @@
 class Element:
     def data(self):
-        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\nType: {self.type}")
+        print(f"Name: {self.name}\nChemical Symbol: {self.chemical_symbol}\nAtomic Mass: {self.atomic_mass}\nAtomic Number: {self.atomic_number}\nSeries: {self.series}")
 
 
 class Hydrogen(Element):
@@ -8,501 +8,508 @@ class Hydrogen(Element):
     chemical_symbol = "H"
     atomic_mass = 1.0079
     atomic_number = 1
-    type = "Reactive Metals (Natrual)"
+    series = "Reactive Metals (Natrual)"
 
 class Helium(Element):
     name = "Helium"
     chemical_symbol = "He"
     atomic_mass = 4.0026
     atomic_number = 2
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Lithium(Element):
     name = "Lithium"
     chemical_symbol = "Li"
     atomic_mass = 6.941
     atomic_number = 3
-    type = "Reactive Metals (Natrual)"
+    series = "Reactive Metals (Natrual)"
 
 class Beryllium(Element):
     name = "Beryllium"
     chemical_symbol = "Be"
     atomic_mass = 9.0122
     atomic_number = 4
-    type = "Reactive Metals (Natrual)"
+    series = "Reactive Metals (Natrual)"
 
 class Boron(Element):
     name = "Boron"
     chemical_symbol = "B"
     atomic_mass = 10.811
     atomic_number = 5
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Carbon(Element):
     name = "Carbon"
     chemical_symbol = "C"
     atomic_mass = 12.011
     atomic_number = 6
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Nitrogen(Element):
     name = "Nitrogen"
     chemical_symbol = "N"
     atomic_mass = 14.007
     atomic_number = 7
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Oxygen(Element):
     name = "Oxygen"
     chemical_symbol = "O"
     atomic_mass = 15.999
     atomic_number = 8
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Fluorine(Element):
     name = "Fluorine"
     chemical_symbol = "F"
     atomic_mass = 18.998
     atomic_number = 9
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Neon(Element):
     name = "Neon"
     chemical_symbol = "Ne"
     atomic_mass = 20.180
     atomic_number = 10
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Sodium(Element):
     name = "Sodium"
     chemical_symbol = "Na"
     atomic_mass = 22.990
     atomic_number = 11
-    type = "Reactive Metals (Natrual)"
+    series = "Reactive Metals (Natrual)"
 
 class Magnesium(Element):
     name = "Magnesium"
     chemical_symbol = "Mg"
     atomic_mass = 24.305
     atomic_number = 12
-    type = "Reactive Metals (Natrual)"
+    series = "Reactive Metals (Natrual)"
 
 class Aluminum(Element):
     name = "Aluminum"
     chemical_symbol = "Al"
     atomic_mass = 26.982
     atomic_number = 13
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Silicon(Element):
     name = "Silicon"
     chemical_symbol = "Si"
     atomic_mass = 28.086
     atomic_number = 14
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Phosphorus(Element):
     name = "Phosphorus"
     chemical_symbol = "P"
     atomic_mass = 30.974
     atomic_number = 15
-    type = "Mainly Non-metals (Natrural)"
+    series = "Mainly Non-metals (Natrural)"
 
 class Sulfur(Element):
     name = "Sulfur"
     chemical_symbol = "S"
     atomic_mass = 32.065
     atomic_number = 16
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Chlorine(Element):
     name = "Chlorine"
     chemical_symbol = "Cl"
     atomic_mass = 35.453
     atomic_number = 17
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Argon(Element):
     name = "Argon"
     chemical_symbol = "Ar"
     atomic_mass = 39.948
     atomic_number = 18
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Potassium(Element):
     name = "Potassium"
     chemical_symbol = "K"
     atomic_mass = 39.098
     atomic_number = 19
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Calcium(Element):
     name = "Calcium"
     chemical_symbol = "Ca"
     atomic_mass = 40.078
     atomic_number = 20
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Scandium(Element):
     name = "Scandium"
     chemical_symbol = "Sc"
     atomic_mass = 44.956
     atomic_number = 21
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Titanium(Element):
     name = "Titanium"
     chemical_symbol = "Ti"
     atomic_mass = 47.867
     atomic_number = 22
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Vanadium(Element):
     name = "Vanadium"
     chemical_symbol = "V"
     atomic_mass = 50.942
     atomic_number = 23
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Chronium(Element):
     name = "Chronium"
     chemical_symbol = "Cr"
     atomic_mass = 51.996
     atomic_number = 24
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Maganese(Element):
     name = "Maganese"
     chemical_symbol = "Mn"
     atomic_mass = 54.938
     atomic_number = 25
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Iron(Element):
     name = "Iron"
     chemical_symbol = "Fe"
     atomic_mass = 55.845
     atomic_number = 26
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Cobalt(Element):
     name = "Cobalt"
     chemical_symbol = "Co"
     atomic_mass = 58.933
     atomic_number = 27
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Nickel(Element):
     name = "Nickel"
     chemical_symbol = "Ni"
     atomic_mass = 58.693
     atomic_number = 28
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Copper(Element):
     name = "Copper"
     chemical_symbol = "Cu"
     atomic_mass = 63.546
     atomic_number = 29
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Zinc(Element):
     name = "Zinc"
     chemical_symbol = "Zn"
     atomic_mass = 65.39
     atomic_number = 30
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Gallium(Element):
     name = "Gallium"
     chemical_symbol = "Ga"
     atomic_mass = 69.723
     atomic_number = 31
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Germanium(Element):
     name = "Germanium"
     chemical_symbol = "Ge"
     atomic_mass = 72.64
     atomic_number = 32
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Arsenic(Element):
     name = "Arsenic"
     chemical_symbol = "As"
     atomic_mass = 74.922
     atomic_number = 33
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Selenium(Element):
     name = "Selenium"
     chemical_symbol = "Se"
     atomic_mass = 78.96
     atomic_number = 34
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Bromine(Element):
     name = "Bromine"
     chemical_symbol = "Br"
     atomic_mass = 79.904
     atomic_number = 35
-    type = "Mainly Non-metals (Natrual)"
+    series = "Mainly Non-metals (Natrual)"
 
 class Krypton(Element):
     name = "Krypton"
     chemical_symbol = "Kr"
     atomic_mass = 83.80
     atomic_number = 36
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Rubidium(Element):
     name = "Rubidium"
     chemical_symbol = "Rb"
     atomic_mass = 85.468
     atomic_number = 37
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Strontium(Element):
     name = "Strontium"
     chemical_symbol = "Sr"
     atomic_mass = 87.62
     atomic_number = 38
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Yttrium(Element):
     name = "Yttrium"
     chemical_symbol = "Y"
     atomic_mass = 88.906
     atomic_number = 39
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Zirconium(Element):
     name = "Zirconium"
     chemical_symbol = "Zr"
     atomic_mass = 91.224
     atomic_number = 40
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Niobium(Element):
     name = "Niobium"
     chemical_symbol = "Nb"
     atomic_mass = 92.906
     atomic_number = 41
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Molybdenum(Element):
     name = "Molybdenum"
     chemical_symbol = "Mo"
     atomic_mass = 95.94
     atomic_number = 42
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Technetium(Element):
     name = "Technetium"
     chemical_symbol = "Tc"
     atomic_mass = 96
     atomic_number = 43
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Ruthenium(Element):
     name = "Ruthenium"
     chemical_symbol = "Ru"
     atomic_mass = 101.07
     atomic_number = 44
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Rhodium(Element):
     name = "Rhodium"
     chemical_symbol = "Rh"
     atomic_mass = 102.91
     atomic_number = 45
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Palladium(Element):
     name = "Palladium"
     chemical_symbol = "Pd"
     atomic_mass = 106.42
     atomic_number = 46
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Silver(Element):
     name = "Silver"
     chemical_symbol = "Ag"
     atomic_mass = 107.87
     atomic_number = 47
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Cadmium(Element):
     name = "Cadmium"
     chemical_symbol = "Cd"
     atomic_mass = 15.999
     atomic_number = 48
-    type = "Transition Metals (Natural)"
+    series = "Transition Metals (Natural)"
 
 class Indium(Element):
     name = "Indium"
     chemical_symbol = "In"
     atomic_mass = 114.82
     atomic_number = 49
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Tin(Element):
     name = "Tin"
     chemical_symbol = "Sn"
     atomic_mass = 118.71
     atomic_number = 50
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Antimony(Element):
     name = "Antimony"
     chemical_symbol = "Sb"
     atomic_mass = 121.76
     atomic_number = 51
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Tellurium(Element):
     name = "Tellurium"
     chemical_symbol = "Te"
     atomic_mass = 127.60
     atomic_number = 52
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Iodine(Element):
     name = "Iodine"
     chemical_symbol = "I"
     atomic_mass = 126.90
     atomic_number = 53
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Xenon(Element):
     name = "Xenon"
     chemical_symbol = "Xe"
     atomic_mass = 131.29
     atomic_number = 54
-    type = "Mainly Non-metals (Natural)"
+    series = "Mainly Non-metals (Natural)"
 
 class Caesium(Element):
     name = "Caesium"
     chemical_symbol = "Cs"
     atomic_mass = 132.91
     atomic_number = 55
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Barium(Element):
     name = "Barium"
     chemical_symbol = "Ba"
     atomic_mass = 137.33
     atomic_number = 56
-    type = "Reactive Metals (Natural)"
+    series = "Reactive Metals (Natural)"
 
 class Lathanium(Element):
     name = "Lathanium"
     chemical_symbol = "La"
     atomic_mass = 138.91
     atomic_number = 57
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Cerium(Element):
     name = "Cerium"
     chemical_symbol = "Ce"
     atomic_mass = 140.12
     atomic_number = 58
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Praseodymium(Element):
     name = "Praseodymium"
     chemical_symbol = "Pr"
     atomic_mass = 140.91
     atomic_number = 59
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Neodymium(Element):
     name = "Neodymium"
     chemical_symbol = "Nd"
     atomic_mass = 144.24
     atomic_number = 60
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Promethium(Element):
     name = "Promethium"
     chemical_symbol = "Pm"
     atomic_mass = 145
     atomic_number = 61
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Samarium(Element):
     name = "Samarium"
     chemical_symbol = "Sm"
     atomic_mass = 150.36
     atomic_number = 62
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Europium(Element):
     name = "Europium"
     chemical_symbol = "Eu"
     atomic_mass = 151.96
     atomic_number = 63
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Gadolinium(Element):
     name = "Gadolinium"
     chemical_symbol = "Gd"
     atomic_mass = 157.25
     atomic_number = 64
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Terbium(Element):
     name = "Terbium"
     chemical_symbol = "Tb"
     atomic_mass = 158.93
     atomic_number = 65
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Dysprosium(Element):
     name = "Dysprosium"
     chemical_symbol = "Dy"
     atomic_mass = 162.5
     atomic_number = 66
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Holmium(Element):
     name = "Holmium"
     chemical_symbol = "Ho"
     atomic_mass = 164.93
     atomic_number = 67
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Erbium(Element):
     name = "Erbium"
     chemical_symbol = "Er"
     atomic_mass = 167.26
     atomic_number = 68
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Thulium(Element):
     name = "Thulium"
     chemical_sumbol = "Tm"
     atomic_mass = 168.93
     atomic_number = 69
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Ytterbium(Element):
     name = "Ytterbium"
     chemical_symbol = "Yb"
     atomic_mass = 173.04
     atomic_number = 70
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Lutetium(Element):
     name = "Lutetium"
     chemical_symbol = "Lu"
     atomic_mass = 174.97
     atomic_number = 71
-    type = "Rare Earth Metals (Natural)"
+    series = "Rare Earth Metals (Natural)"
 
 class Hafnium(Element):
     name = "Hafnium"
     chemical_symbol = "Hf"
     atomic_mass = 178.49
     atomic_number = 72
-    type = "Transition Metals"
+    series = "Transition Metals"
+
+class Tantalum(Element):
+    name = "Tantalum"
+    chemical_symbol = "Ta"
+    atomic_mass = 180.95
+    atomic_number = 73
+    series = "Transition Metals"
